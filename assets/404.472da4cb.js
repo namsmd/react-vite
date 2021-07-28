d00e0370e2b08c3e0e0ceeb0bf126832a83443be
Nam <ntp96dev@gmail.com>
2021-07-28 15:17:19 +0700

    Updates

---
+++ assets/404.472da4cb.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.a2e7fa00.js";export default()=>e.createElement("div",null,"Not Found");
---
+++ assets/Button.3697d8f2.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.a2e7fa00.js";const t=({onClick:t,type:o="button",children:n})=>e.createElement("button",{type:o,className:"w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm",onClick:t},n);export{t as B};
